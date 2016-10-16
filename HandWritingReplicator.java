43206b5670f133e66df52c6ad7148c81e02560bd
Jobin Johnson <jobinrjohnson@gmail.com>
2016-10-16 07:07:26 +0530

    initial commit

--- HandWritingReplicator.java
+++ HandWritingReplicator.java
@@ -10,7 +10,7 @@ public class HandWritingReplicator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         new Testing().setVisible(true);
     }
     
